3933ad66b0fccc43c8b3d75fb50ad6f7ae0ae6b4
devch1013 <devch1013@gmail.com>
2025-02-17 22:21:56 +0900

    테스트 코드 변경

--- api/event/tests/test_event_view.py
+++ api/event/tests/test_event_view.py
@@ -104,38 +104,4 @@ class EventViewSetTests(APITestCase):
         self.assertEqual(Event.objects.first().location, "Updated Location")
         self.assertEqual(len(Event.objects.first().images), 2)
 
-    @patch("django.core.files.storage.default_storage.save")
-    def test_attendance_check(self, mock_storage):
-        """정상 출석 체크"""
-        mock_storage.return_value = "test-image-path.jpg"
-
-        now = timezone.localtime(timezone.now())
-        end_time = (now + timedelta(hours=1)).time()
-
-        event = Event.objects.create(
-            title="Test Event",
-            description="Test Event Description",
-            location="Test Location",
-            images=[ImageTestUtils.create_test_image()],
-            generation=self.club.current_generation,
-            date=now.date(),
-            start_time=now.time(),
-            end_time=end_time,
-            start_minutes=-10,
-            late_minutes=10,
-            fail_minutes=30,
-            qr_code="123456",
-            qr_code_url=None,
-        )
-
-        response = self.client.post(
-            reverse("event-attendance", kwargs={"event_id": event.id}),
-            data={
-                "event_id": event.id,
-                "qr_code": event.qr_code,
-                "latitude": 35.123456,
-                "longitude": 129.123456,
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["status"], AttendanceStatus.PRESENT.value)
+    

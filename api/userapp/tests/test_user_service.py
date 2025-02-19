5e4aba203f897b25fde1d55958c52d159736b25a
devch1013 <devch1013@gmail.com>
2025-02-19 10:27:34 +0900

    user 테스트 수정

--- api/userapp/tests/test_user_service.py
+++ api/userapp/tests/test_user_service.py
@@ -1,12 +1,9 @@
-import io
-
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
-from PIL import Image
 from rest_framework import status
 from rest_framework.test import APITestCase
-
+from unittest.mock import patch
 from api.userapp.models import User
+from common.test_utils.image_utils import ImageTestUtils
 
 
 class UserViewTest(APITestCase):
@@ -16,7 +13,7 @@ class UserViewTest(APITestCase):
             identifier=self.test_identifier, username="Test User"
         )
         self.client.force_authenticate(user=self.user)
-        self.url = reverse("user_me")
+        self.url = reverse("user-me")
 
     def test_retrieve_user(self):
         """사용자 정보 조회 테스트"""
@@ -28,23 +25,16 @@ class UserViewTest(APITestCase):
         self.assertEqual(response.data["phone_number"], None)
         self.assertEqual(response.data["profile_image"], None)
 
-    def test_update_user(self):
+    @patch("django.core.files.storage.default_storage.save")
+    def test_update_user(self, mock_storage):
         """사용자 정보 업데이트 테스트"""
-        # Create a test image using PIL
-        image = Image.new("RGB", (100, 100), color="red")
-        image_io = io.BytesIO()
-        image.save(image_io, format="PNG")
-        image_io.seek(0)
-
-        test_image = SimpleUploadedFile(
-            name="test.png", content=image_io.getvalue(), content_type="image/png"
-        )
+        mock_storage.return_value = "test-image-path.jpg"
 
         update_data = {
             "username": "Updated Username",
             "email": "test@example.com",
             "phone_number": "010-1234-5678",
-            "profile_image": test_image,
+            "profile_image": ImageTestUtils.create_test_image(),
         }
 
         response = self.client.put(self.url, update_data, format="multipart")
@@ -61,8 +51,8 @@ class UserViewTest(APITestCase):
         self.assertEqual(self.user.username, "Updated Username")
         self.assertEqual(self.user.email, "test@example.com")
         self.assertEqual(self.user.phone_number, "010-1234-5678")
-        self.assertTrue(response.data["profile_image"].startswith("https://"))
-        self.assertTrue(response.data["profile_image"].endswith(".png"))
+        self.assertTrue(self.user.profile_image.url.startswith("https://"))
+        self.assertTrue(self.user.profile_image.url.endswith(".png"))
 
     def test_update_user_unauthenticated(self):
         """인증되지 않은 사용자의 업데이트 시도 테스트"""

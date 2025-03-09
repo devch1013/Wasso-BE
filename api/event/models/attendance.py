7f9ff3faf90917f7e43be20f35cab937e07b98d6
devch1013 <devch1013@gmail.com>
2025-03-09 20:13:33 +0900

    model __str__ 추가

--- api/event/models/attendance.py
+++ api/event/models/attendance.py
@@ -8,6 +8,10 @@ from .event import Event
 
 
 class Attendance(models.Model):
+    
+    def __str__(self):
+        return f"{self.generation_mapping.member.user.username} - {self.event.title} - {AttendanceStatus(self.status).label}"
+    
     generation_mapping = models.ForeignKey(GenMember, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     status = models.IntegerField(
